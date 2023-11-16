786bbdf366cd1bf548944e896b350d070fdb1849
Guilherme Veloso <100156111+guigvd@users.noreply.github.com>
2023-11-15 21:24:50 -0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,16 +1,30 @@
 # example/views.py
-from datetime import datetime
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time iss { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+def home(request):
+    if request.method == 'POST':
+        # PEGA OS VALORES DO FORM
+        meta_str = request.POST.get('meta')
+        economia_str = request.POST.get('economia')
+
+        try:
+        # CONVERTER EM NUMERO
+            meta = float(meta_str)
+            economia = float(economia_str)
+
+            if (meta != 0) or (economia != 0):
+                res =  float(meta) / float(economia)
+                resultado = f'Levará {res} meses até atingir a meta!'
+            else:
+                resultado = 'Os valores não podem ser zero.'
+
+            if economia > meta:
+                resultado = 'A economia não pode ser maior que a meta.'
+
+        except ValueError:
+            resultado = 'Insira apenas números.'
+
+        return render(request, 'index.html', {'resultado': resultado})
+
+    return render(request, 'index.html')
